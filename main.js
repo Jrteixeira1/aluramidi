fef2f7b2b33293e7d7d1183f8e6a303e6791d8c6
Júnior Teixeira <106847834+Jrteixeira1@users.noreply.github.com>
2023-06-24 21:51:27 -0300

    troca da função

--- main.js
+++ main.js
@@ -5,18 +5,31 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-//enquanto
-while (contador < listaDeTeclas.length) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-//template string
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
-    };
+    }
 
-    contador = contador + 1;
+ }
+ 
+
+ //enquanto
+ //let contador = 0;
+//while (contador < listaDeTeclas.length) {
+ //   const tecla = listaDeTeclas[contador];
+  //  const instrumento = tecla.classList[1];
+    //template string
+   // const idAudio = `#som_${instrumento}`;
+
+  //  tecla.onclick = function () {
+   //     tocaSom(idAudio);
+   // };
+
+   // contador = contador + 1;
  }
