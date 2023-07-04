9f1a5f8bf4648aca7f03e0b6637035db588ea1f5
Júnior Teixeira <106847834+Jrteixeira1@users.noreply.github.com>
2023-07-04 12:58:23 -0300

    CORREÇÃO

--- main.js
+++ main.js
@@ -10,24 +10,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    const idAudio = `#som_${instrumento}`; 
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
  }
-  //enquanto
- //let contador = 0;
-//while (contador < listaDeTeclas.length) {
- //   const tecla = listaDeTeclas[contador];
-  //  const instrumento = tecla.classList[1];
-    //template string
-   // const idAudio = `#som_${instrumento}`;
-
-  //  tecla.onclick = function () {
-   //     tocaSom(idAudio);
-   // };
-
-   // contador = contador + 1;
- // }
